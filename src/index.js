e7447e6e06cd913ed596fe091c016d50992d99c7
ivan1986mrpl <121858281+ivan1986mrpl@users.noreply.github.com>
2025-08-20 12:25:53 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
-module.exports = function check(/* str, bracketsConfig */) {
-  throw new Error('Not implemented');
+module.exports = function check(str, bracketsConfig) {
+  const stack = [];
+
+  const openToClose = new Map();
+  const openBrackets = new Set();
+  const sameBrackets = new Set();
+
+  bracketsConfig.forEach(([open, close]) => {
+    openToClose.set(open, close);
+    openBrackets.add(open);
+    if (open === close) {
+      sameBrackets.add(open);
+    }
+  });
+
+  let isValid = true;
+
+  str.split('').some((char) => {
+    if (!isValid) return true;
+
+    if (sameBrackets.has(char)) {
+      if (stack.length > 0 && stack[stack.length - 1] === char) {
+        stack.pop();
+      } else {
+        stack.push(char);
+      }
+    } else if (openBrackets.has(char)) {
+      stack.push(char);
+    } else {
+      if (stack.length === 0) {
+        isValid = false;
+        return true;
+      }
+      const lastOpen = stack[stack.length - 1];
+      if (openToClose.get(lastOpen) === char) {
+        stack.pop();
+      } else {
+        isValid = false;
+        return true;
+      }
+    }
+    return false;
+  });
+
+  return isValid && stack.length === 0;
 };
